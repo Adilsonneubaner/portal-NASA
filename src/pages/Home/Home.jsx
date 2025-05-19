caaa2ae5441e5231eafbce2caa066918520761d3
Adilson Neubaner <151559307+Adilsonneubaner@users.noreply.github.com>
2025-05-19 20:24:41 -0300

    feat:adicionando conteúdo da home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,21 @@
 import './Home.css'
 
+import nasa from '../../images/logo-nasa.jpg'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+      <div className="container-home">
+        <h1>DESLUMBRE O UNIVERSO E SUAS MARAVILHAS</h1>
+        <div className="content-home">
+          <div className="paragraph-home">
+            <p>A agência do governo dos Estados Unidos responsável pela pesquisa e desenvolvimento de tecnologias e programas de exoploração espacial, a NASA(National Aeronautics and Space Administration), fornece para uso público algumas de suas informações adquiridas a partir de seus estudos e explorações. Informações essas que para você amante de astronomia, são um prato cheio.</p>
+            <p>Então não perca tempo, venha matar sua sede de curiosidade, e se encantar com algumas images impressionantes.</p>
+          </div>
+          <img src={nasa} alt={nasa} className='image-home'/>
+        </div>
+      </div>
+    </main>
   )
 }
 
