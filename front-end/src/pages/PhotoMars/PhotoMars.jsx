f1ae378187948ca48eb9a23d28bdd008f5daa35c
Adilson Neubaner <151559307+Adilsonneubaner@users.noreply.github.com>
2025-06-02 21:05:22 -0300

    feat(photo mars): adicionando conteúdo html base

--- front-end/src/pages/PhotoMars/PhotoMars.jsx
+++ front-end/src/pages/PhotoMars/PhotoMars.jsx
@@ -1,8 +1,95 @@
 import './PhotoMars.css'
 
 const PhotoMars = () => {
+
+  const handleCuriosity = (e) => {
+    e.preventDefault()
+  }
+
+  const handleOpportunity = (e) => {
+    e.preventDefault()
+  }
+
+  const handleSpirit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>PhotoMars</div>
+    <main>
+      <div className="container-content margin-containers">
+          <div className="container-rovers">
+              <form>
+                <div className="rovers-box">
+                  Curiosity
+                  <input type="radio" name="rovers" id="curiosity" />
+                </div>
+
+                <div className="rovers-box">
+                  Opportunity
+                  <input type="radio" name="rovers" id="opportunity" />
+                </div>
+
+                <div className="rovers-box">
+                  Spirit
+                  <input type="radio" name="rovers" id="spirit" />
+                </div>
+              </form>
+          </div>
+
+          <div className="rovers-data">
+            <div id="curiosity-data">
+              <p>Dados sobre</p>
+              <p>Data de lançamento</p>
+              <p>Data de pouso</p>
+              <p>Status</p>
+              <p>Data das últimas fotos</p>
+              <p>Fotos já tiradas</p>
+
+              <form onSubmit={handleCuriosity}>
+                <label>
+                  Busque fotos por uma data
+                  <input type="date" id="" />
+                </label>
+                <button type='submit'>Veja fotos</button>
+              </form>
+            </div>
+
+            <div id="opportunity-data">
+              <p>Dados sobre</p>
+              <p>Data de lançamento</p>
+              <p>Data de pouso</p>
+              <p>Status</p>
+              <p>Data das últimas fotos</p>
+              <p>Fotos já tiradas</p>
+
+              <form onSubmit={handleOpportunity}>
+                <label>
+                  Busque fotos por uma data
+                  <input type="date" id="" />
+                </label>
+                <button type='submit'>Veja fotos</button>
+              </form>
+            </div>
+          
+            <div id="spirit-data">
+              <p>Dados sobre</p>
+              <p>Data de lançamento</p>
+              <p>Data de pouso</p>
+              <p>Status</p>
+              <p>Data das últimas fotos</p>
+              <p>Fotos já tiradas</p>
+
+              <form onSubmit={handleSpirit}>
+                <label>
+                  Busque fotos por uma data
+                  <input type="date" id="" />
+                </label>
+                <button type='submit'>Veja fotos</button>
+              </form>
+            </div>
+          </div>
+      </div>
+    </main>
   )
 }
 
