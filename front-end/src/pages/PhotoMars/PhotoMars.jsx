f4a4e177c209585ed04564f6ce398d1d55329ea6
Adilson Neubaner <151559307+Adilsonneubaner@users.noreply.github.com>
2026-05-28 23:03:18 -0300

    feat(api descontinuada): alterando os manifest

--- front-end/src/pages/PhotoMars/PhotoMars.jsx
+++ front-end/src/pages/PhotoMars/PhotoMars.jsx
@@ -15,70 +15,30 @@ import Loading from '../../components/Loading/Loading'
 
 const PhotoMars = () => {
   const {loading, setLoading} = useContext(LoadingContext)
-  
-  const api_key_nasa = 'XdPKGfLWgyAVR6bvL6vDO6etSOec8xJknUoehHmK'
 
-  const urlManifestCuriosity = `https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity?api_key=${api_key_nasa}`
+  const manifestCuriosity = {
+    name: "Curiosity",
+    launch_date: "26-11-2011",
+    landing_date: "06-08-2012",
+    status: "missão ativa",
+    total_photos: "650.000+"
+  }
 
-  const urlManifestOpportunity = `https://api.nasa.gov/mars-photos/api/v1/manifests/Opportunity?api_key=${api_key_nasa}`
+  const manifestOpportunity = {
+    name: "Opportunity",
+    launch_date: "07-07-2003",
+    landing_date: "26-01-2004",
+    status: "missão completa",
+    total_photos: "228.779"
+  }
 
-  const urlManifestSpirit = `https://api.nasa.gov/mars-photos/api/v1/manifests/Spirit?api_key=${api_key_nasa}`
-
-  const {data: manifestCuriosity} = useGet(urlManifestCuriosity)
-  const {data: manifestOpportunity} = useGet(urlManifestOpportunity)
-  const {data: manifestSpirit} = useGet(urlManifestSpirit)
-
-
-  // Curiosity date transcribed
-  const {transcribedDate: curiosity_launch_date} = useTranscribedDate(manifestCuriosity ? manifestCuriosity.photo_manifest.launch_date : null)
-
-  const {transcribedDate: curiosity_landing_date} = useTranscribedDate(manifestCuriosity ? manifestCuriosity.photo_manifest.landing_date : null)
-
-  const {transcribedDate: curiosity_max_date} = useTranscribedDate(manifestCuriosity ? manifestCuriosity.photo_manifest.max_date : null)
-  
-  // Opportunity date transcribed
-  const {transcribedDate: opportunity_launch_date} = useTranscribedDate(manifestOpportunity ? manifestOpportunity.photo_manifest.launch_date : null)
-
-  const {transcribedDate: opportunity_landing_date} = useTranscribedDate(manifestOpportunity ? manifestOpportunity.photo_manifest.landing_date : null)
-
-  const {transcribedDate: opportunity_max_date} = useTranscribedDate(manifestOpportunity ? manifestOpportunity.photo_manifest.max_date : null)
-
-  // Spirit date transcribed
-  const {transcribedDate: spirit_launch_date} = useTranscribedDate(manifestSpirit ? manifestSpirit.photo_manifest.launch_date : null)
-
-  const {transcribedDate: spirit_landing_date} = useTranscribedDate(manifestSpirit ? manifestSpirit.photo_manifest.landing_date : null)
-
-  const {transcribedDate: spirit_max_date} = useTranscribedDate(manifestSpirit ? manifestSpirit.photo_manifest.max_date : null)
-
-
-  // Translations
-  const {translation: translationCuriosity} = useTranslate(manifestCuriosity? [manifestCuriosity.photo_manifest.status] : null)
-
-  const {translation: translationOpportunity} = useTranslate(manifestOpportunity? [manifestOpportunity.photo_manifest.status] : null)
-
-  const {translation: translationSpirit} = useTranslate(manifestSpirit? [manifestSpirit.photo_manifest.status] : null)
-
-  // Array de depedências para Loading
-  const allDeps = [
-    curiosity_landing_date,
-    curiosity_launch_date,
-    curiosity_max_date,
-    opportunity_landing_date,
-    opportunity_launch_date,
-    opportunity_max_date,
-    spirit_landing_date,
-    spirit_launch_date,
-    spirit_max_date,
-    translationCuriosity,
-    translationOpportunity,
-    translationSpirit
-  ]
-
-  useEffect(() => {
-    if(allDeps.every(Boolean)) {
-      setLoading(false)
-    }
-  }, [allDeps])
+  const manifestSpirit = {
+    name: "Spirit",
+    launch_date: "10-06-2003",
+    landing_date: "04-01-2004",
+    status: "missão completa",
+    total_photos: "124.838"
+  }
 
 
   const [selected, setSelected] = useState('')
@@ -166,120 +126,99 @@ const PhotoMars = () => {
           </form>
 
           <div className="rovers-data">
-            {manifestCuriosity && translationCuriosity && (
+            {selected === 'curiosity' && (
               <>
-                {selected === 'curiosity' && (
-                  <>
-                    <h2>{manifestCuriosity.photo_manifest.name}</h2>
-                    <div className="data-form">
+                <h2>{manifestCuriosity.name}</h2>
+                <div className="data-form">
 
-                      <div className="rovers-information-container">
-                        <p className="color-text">Data de lançamento
-                          <span className="color-data"> {curiosity_launch_date}</span>
-                        </p>
-                        <p className="color-text">Data de pouso
-                          <span className="color-data"> {curiosity_landing_date}</span>
-                        </p>
-                        <p className="color-text">Status
-                          <span className="color-data"> {translationCuriosity.translations[0].text}</span>
-                        </p>
-                        <p className="color-text">Data das últimas fotos
-                          <span className="color-data"> {curiosity_max_date}</span>
-                        </p>
-                        <p className="color-text">Fotos já tiradas
-                          <span className="color-data"> {manifestCuriosity.photo_manifest.total_photos}</span>
-                        </p>
-                      </div>
+                  <div className="rovers-information-container">
+                    <p className="color-text">Data de lançamento
+                      <span className="color-data"> {manifestCuriosity.launch_date}</span>
+                    </p>
+                    <p className="color-text">Data de pouso
+                      <span className="color-data"> {manifestCuriosity.landing_date}</span>
+                    </p>
+                    <p className="color-text">Status
+                      <span className="color-data"> {manifestCuriosity.status}</span>
+                    </p>
+                    <p className="color-text">Fotos já tiradas
+                      <span className="color-data"> {manifestCuriosity.total_photos}</span>
+                    </p>
+                  </div>
 
-                      <form onSubmit={(e) => handlePhotos(e, 'curiosity')} className='form-calendar'>
-                        <p className="search-date">Busque fotos por uma data</p> 
-                        <div className="calendar">
-                          <input type="date" id="" onChange={(e) => setDate(e.target.value)}/>
-                          <i className="bi bi-calendar"></i>
-                        </div>
-                        <button type='submit' className="button-submit">Veja fotos</button>
-                      </form>
+                  <form onSubmit={(e) => handlePhotos(e, 'curiosity')} className='form-calendar'>
+                    <p className="search-date">Busque fotos por uma data</p> 
+                    <div className="calendar">
+                      <input type="date" id="" onChange={(e) => setDate(e.target.value)}/>
+                      <i className="bi bi-calendar"></i>
                     </div>
-                  </>
-                )}
+                    <button type='submit' className="button-submit">Veja fotos</button>
+                  </form>
+                </div>
               </>
             )}
 
-            {manifestOpportunity && translationOpportunity && (
+            {selected === 'opportunity' && (
               <>
-                {selected === 'opportunity' && (
-                  <>
-                    <h2>{manifestOpportunity.photo_manifest.name}</h2>
-                    <div className="data-form">
+                <h2>{manifestOpportunity.name}</h2>
+                <div className="data-form">
 
-                      <div className="rovers-information-container">
-                        <p className="color-text">Data de lançamento
-                          <span className="color-data"> {opportunity_launch_date}</span>
-                        </p>
-                        <p className="color-text">Data de pouso
-                          <span className="color-data"> {opportunity_landing_date}</span>
-                        </p>
-                        <p className="color-text">Status
-                          <span className="color-data"> {translationOpportunity.translations[0].text}</span>
-                        </p>
-                        <p className="color-text">Data das últimas fotos
-                          <span className="color-data"> {opportunity_max_date}</span>
-                        </p>
-                        <p className="color-text">Fotos já tiradas
-                          <span className="color-data"> {manifestOpportunity.photo_manifest.total_photos}</span>
-                        </p>
-                      </div>
+                  <div className="rovers-information-container">
+                    <p className="color-text">Data de lançamento
+                      <span className="color-data"> {manifestOpportunity.launch_date}</span>
+                    </p>
+                    <p className="color-text">Data de pouso
+                      <span className="color-data"> {manifestOpportunity.landing_date}</span>
+                    </p>
+                    <p className="color-text">Status
+                      <span className="color-data"> {manifestOpportunity.status}</span>
+                    </p>
+                    <p className="color-text">Fotos já tiradas
+                      <span className="color-data"> {manifestOpportunity.total_photos}</span>
+                    </p>
+                  </div>
 
-                      <form onSubmit={(e) => handlePhotos(e, 'opportunity')} className='form-calendar'>
-                        <p className="search-date">Busque fotos por uma data</p> 
-                        <div className="calendar">
-                          <input type="date" id="" onChange={(e) => setDate(e.target.value)}/>
-                          <i className="bi bi-calendar"></i>
-                        </div>
-                        <button type='submit' className="button-submit">Veja fotos</button>
-                      </form>
+                  <form onSubmit={(e) => handlePhotos(e, 'opportunity')} className='form-calendar'>
+                    <p className="search-date">Busque fotos por uma data</p> 
+                    <div className="calendar">
+                      <input type="date" id="" onChange={(e) => setDate(e.target.value)}/>
+                      <i className="bi bi-calendar"></i>
                     </div>
-                  </>
-                )}
+                    <button type='submit' className="button-submit">Veja fotos</button>
+                  </form>
+                </div>
               </>
             )}
           
-            {manifestSpirit && translationSpirit && (
+            {selected === 'spirit' && (
               <>
-                {selected === 'spirit' && (
-                  <>
-                  <h2>{manifestSpirit.photo_manifest.name}</h2>
-                  <div className="data-form">
+              <h2>{manifestSpirit.name}</h2>
+              <div className="data-form">
 
-                      <div className="rovers-information-container">
-                        <p className="color-text">Data de lançamento
-                          <span className="color-data"> {spirit_launch_date}</span>
-                        </p>
-                        <p className="color-text">Data de pouso
-                          <span className="color-data"> {spirit_landing_date}</span>
-                        </p>
-                        <p className="color-text">Status
-                          <span className="color-data"> {translationSpirit.translations[0].text}</span>
-                        </p>
-                        <p className="color-text">Data das últimas fotos
-                          <span className="color-data"> {spirit_max_date}</span>
-                        </p>
-                        <p className="color-text">Fotos já tiradas
-                          <span className="color-data"> {manifestSpirit.photo_manifest.total_photos}</span>
-                        </p>
-                      </div>
+                  <div className="rovers-information-container">
+                    <p className="color-text">Data de lançamento
+                      <span className="color-data"> {manifestSpirit.launch_date}</span>
+                    </p>
+                    <p className="color-text">Data de pouso
+                      <span className="color-data"> {manifestSpirit.launch_date}</span>
+                    </p>
+                    <p className="color-text">Status
+                      <span className="color-data"> {manifestSpirit.status}</span>
+                    </p>
+                    <p className="color-text">Fotos já tiradas
+                      <span className="color-data"> {manifestSpirit.total_photos}</span>
+                    </p>
+                  </div>
 
-                      <form onSubmit={(e) => handlePhotos(e, 'spirit')} className='form-calendar'>
-                        <p className="search-date">Busque fotos por uma data</p> 
-                        <div className="calendar">
-                          <input type="date" id="" onChange={(e) => setDate(e.target.value)}/>
-                          <i className="bi bi-calendar"></i>
-                        </div>
-                        <button type='submit' className="button-submit">Veja fotos</button>
-                      </form>
+                  <form onSubmit={(e) => handlePhotos(e, 'spirit')} className='form-calendar'>
+                    <p className="search-date">Busque fotos por uma data</p> 
+                    <div className="calendar">
+                      <input type="date" id="" onChange={(e) => setDate(e.target.value)}/>
+                      <i className="bi bi-calendar"></i>
                     </div>
-                  </>
-                )}
+                    <button type='submit' className="button-submit">Veja fotos</button>
+                  </form>
+                </div>
               </>
             )}
 
